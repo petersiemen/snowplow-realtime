9069767f7ba9939ce030091bd55163a17b623c58
Peter Siemen <peter.siemen@googlemail.com>
2020-10-30 16:22:50 +0100

    example

--- example.source.sh
+++ example.source.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+export aws_region="eu-central-1"
+export bucket_name="XYZ"
+export artifacts_bucket_name="XYZ"
 
-export TF_master_account_id=123456789
-export TF_production_account_id=123456789
-export TF_dns_zone_id=XYZ
-export TF_domain=yourdomain.com
+export master_account_id=123456
+export master_account_profile=XYZ
+export production_account_id=78901234
+export production_account_profile=ABC
+export shared_services_account_id=56789123
+export shared_services_account_profile=DEF
 
-export TF_vpc_id="vpc-123456"
-export TF_subnet_ids='["subnet-123", "subnet-456", "subnet-789"]'
-export TF_public_key="ssh-rsa XYZ"
+export dns_zone_id=KLMOP
+export domain=YOURDOMAIN.COM
+
+export vpc_id="vpc-124567"
+export vpc_cidr="172.31.0.0/16"
+export subnet_ids='["subnet-123456", "subnet-789123", "subnet-45678"]'
+export route_table_id="rtb-xyz"
+export public_key="ssh-rsa ABCEFESDFGSDF"
+
+export trusted_ip_address="1.2.3.4"
